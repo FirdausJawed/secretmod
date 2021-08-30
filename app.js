1e35b10c655f934a9a9ed26f215af646d3d5746b
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-08-30 18:06:27 +0530

    v_5.0

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
-mongoose.set("userCreateIndex",true);
+//mongoose.set("userCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
    email:String,
@@ -55,22 +55,49 @@ app.get('/register', function(req, res){
 res.render("register");
 });
 
+app.get("/secrets",function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login")
+  }
+});
+
+app.get("/logout",function(req, res){
+  req.logout();
+  res.redirect("/")
+});
+
+
 app.post("/register",function(req,res){
  User.register({username:req.body.username},req.body.password,function(err,user){
    if(err){
      console.log(err);
      res.redirect("/register");
    }else{
-     passport.authenticate("local")()
+     passport.authenticate("local")(req,res,function(){
+       res.redirect("/secrets");
+     })
    }
  })
 });
 
 app.post("/login",function(req,res){
 
-
+  const user = new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
-
 
 
 app.listen(3000, function(){
