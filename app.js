5f9ba776aa33d63695ad62822c55bd46e5622a54
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-08-30 12:51:23 +0530

    v_4.0

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 app.set("views engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,10 +20,6 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
    email:String,
    password:String
  });
-
-
- userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
-
 
 const User =new mongoose.model("User",userSchema);
 
@@ -38,31 +36,35 @@ res.render("register");
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      console.log("secrets");
-    }
+
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("secrets");
+      }
+    });
   });
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
-
+  const password= req.body.password;
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }
+          bcrypt.compare(password,foundUser.password,function(err,result){
+           if(result===true){
+             res.render("secrets");
+           }
+          })
       }
     }
   });
